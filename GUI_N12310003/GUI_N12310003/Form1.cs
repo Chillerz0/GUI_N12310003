6d27336dee26ee645ec2b97773263daca6672a2d
Chillerz0 <zheanli72@gmail.com>
2026-03-17 21:05:07 +0800

    First Commit

--- GUI_N12310003/GUI_N12310003/Form1.cs
+++ GUI_N12310003/GUI_N12310003/Form1.cs
@@ -16,5 +16,10 @@ namespace GUI_N12310003
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
