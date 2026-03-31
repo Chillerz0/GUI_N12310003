017b65adc1f53396f218735d6820b696bb2620b8
Chillerz0 <zheanli72@gmail.com>
2026-03-31 21:09:08 +0800

    20260331

--- GUI_N12310003/GUI_N12310003/Form1.Designer.cs
+++ GUI_N12310003/GUI_N12310003/Form1.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.btn_ClickMe = new System.Windows.Forms.Button();
+            this.lab_Changelabel = new System.Windows.Forms.Label();
+            this.btn_ChangeLabel = new System.Windows.Forms.Button();
+            this.btn_Counter = new System.Windows.Forms.Button();
+            this.lab_Counter = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -49,7 +53,7 @@
             this.btn_ClickMe.BackColor = System.Drawing.Color.LightGray;
             this.btn_ClickMe.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
             this.btn_ClickMe.ForeColor = System.Drawing.Color.SteelBlue;
-            this.btn_ClickMe.Location = new System.Drawing.Point(12, 92);
+            this.btn_ClickMe.Location = new System.Drawing.Point(12, 104);
             this.btn_ClickMe.Name = "btn_ClickMe";
             this.btn_ClickMe.Size = new System.Drawing.Size(319, 70);
             this.btn_ClickMe.TabIndex = 1;
@@ -57,11 +61,59 @@
             this.btn_ClickMe.UseVisualStyleBackColor = false;
             this.btn_ClickMe.Click += new System.EventHandler(this.btn_ClickMe_Click);
             // 
+            // lab_Changelabel
+            // 
+            this.lab_Changelabel.AutoSize = true;
+            this.lab_Changelabel.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lab_Changelabel.Location = new System.Drawing.Point(203, 197);
+            this.lab_Changelabel.Name = "lab_Changelabel";
+            this.lab_Changelabel.Size = new System.Drawing.Size(0, 31);
+            this.lab_Changelabel.TabIndex = 2;
+            this.lab_Changelabel.Click += new System.EventHandler(this.btn_ChangeLabel_Click);
+            // 
+            // btn_ChangeLabel
+            // 
+            this.btn_ChangeLabel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btn_ChangeLabel.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn_ChangeLabel.Location = new System.Drawing.Point(12, 187);
+            this.btn_ChangeLabel.Name = "btn_ChangeLabel";
+            this.btn_ChangeLabel.Size = new System.Drawing.Size(173, 51);
+            this.btn_ChangeLabel.TabIndex = 3;
+            this.btn_ChangeLabel.Text = "按我切換標籤";
+            this.btn_ChangeLabel.UseVisualStyleBackColor = false;
+            this.btn_ChangeLabel.Click += new System.EventHandler(this.btn_ChangeLabel_Click);
+            // 
+            // btn_Counter
+            // 
+            this.btn_Counter.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btn_Counter.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btn_Counter.Location = new System.Drawing.Point(12, 256);
+            this.btn_Counter.Name = "btn_Counter";
+            this.btn_Counter.Size = new System.Drawing.Size(173, 51);
+            this.btn_Counter.TabIndex = 5;
+            this.btn_Counter.Text = "按我+1";
+            this.btn_Counter.UseVisualStyleBackColor = false;
+            this.btn_Counter.Click += new System.EventHandler(this.btn_Counter_Click);
+            // 
+            // lab_Counter
+            // 
+            this.lab_Counter.AutoSize = true;
+            this.lab_Counter.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.lab_Counter.Location = new System.Drawing.Point(203, 266);
+            this.lab_Counter.Name = "lab_Counter";
+            this.lab_Counter.Size = new System.Drawing.Size(28, 31);
+            this.lab_Counter.TabIndex = 4;
+            this.lab_Counter.Text = "0";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(833, 450);
+            this.Controls.Add(this.btn_Counter);
+            this.Controls.Add(this.lab_Counter);
+            this.Controls.Add(this.btn_ChangeLabel);
+            this.Controls.Add(this.lab_Changelabel);
             this.Controls.Add(this.btn_ClickMe);
             this.Controls.Add(this.label1);
             this.ForeColor = System.Drawing.SystemColors.HotTrack;
@@ -76,6 +128,10 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btn_ClickMe;
+        private System.Windows.Forms.Label lab_Changelabel;
+        private System.Windows.Forms.Button btn_ChangeLabel;
+        private System.Windows.Forms.Button btn_Counter;
+        private System.Windows.Forms.Label lab_Counter;
     }
 }
 
